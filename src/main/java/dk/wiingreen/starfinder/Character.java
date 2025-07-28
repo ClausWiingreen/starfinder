64b2330c7218269c6fe466972d46c5c5db7a6952
Claus Ryberg Wiingreen <crwi@netcompany.com>
2025-07-28 10:47:04 +0200

    feat: associate Character entity with User
    Introduced a ManyToOne relationship between Character and User.
    Each character now has an owner field that links to the user who created it.

--- src/main/java/dk/wiingreen/starfinder/Character.java
+++ src/main/java/dk/wiingreen/starfinder/Character.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 import java.util.UUID;
 
@@ -13,6 +14,8 @@ public class Character {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
+    @ManyToOne
+    private User owner;
 
     public void setName(String name) {
         this.name = name;
