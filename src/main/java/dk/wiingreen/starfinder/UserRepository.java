e137c0e7f39a82344ad095af9dd2af30f839b904
Claus Ryberg Wiingreen <crwi@netcompany.com>
2025-07-28 11:02:10 +0200

    feat: add support for looking up current authenticated user
    Introduced CurrentUserService to retrieve the currently authenticated user
    based on Spring Security context. Extended UserRepository with a
    findByUsername method to support this functionality.

--- src/main/java/dk/wiingreen/starfinder/UserRepository.java
+++ src/main/java/dk/wiingreen/starfinder/UserRepository.java
@@ -2,7 +2,9 @@ package dk.wiingreen.starfinder;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
