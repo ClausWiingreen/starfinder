f434070640e3344a0e404bde1e2d86498b2597b7
Claus Ryberg Wiingreen <crwi@netcompany.com>
2025-07-31 12:00:38 +0200

    feat: replaces user manegement with custom solution

---
+++ src/main/java/dk/wiingreen/starfinder/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package dk.wiingreen.starfinder;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+        return new CustomUserDetails(user);
+    }
+
+}
