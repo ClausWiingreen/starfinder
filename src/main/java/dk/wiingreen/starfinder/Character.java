e4a9c49215dd9590c45685f4e1c2012d7a9aef6a
Claus Ryberg Wiingreen <crwi@netcompany.com>
2025-07-31 12:02:04 +0200

    style: corrects generated database names to be consistent

--- src/main/java/dk/wiingreen/starfinder/Character.java
+++ src/main/java/dk/wiingreen/starfinder/Character.java
@@ -1,20 +1,25 @@
 package dk.wiingreen.starfinder;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import java.util.UUID;
 
 @Entity
+@Table(name = "characters")
 class Character {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk__characters__users_owner"))
     private User owner;
 
     void setName(String name) {
