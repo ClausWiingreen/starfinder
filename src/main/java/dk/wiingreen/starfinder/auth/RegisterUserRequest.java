567259fd787494a684d050f78249ac22eddcc039
Claus Ryberg Wiingreen <claus@wiingreen.dk>
2025-08-04 22:45:27 +0200

    feat: add validation to ensure username is not blank during registration

--- src/main/java/dk/wiingreen/starfinder/auth/RegisterUserRequest.java
+++ src/main/java/dk/wiingreen/starfinder/auth/RegisterUserRequest.java
@@ -3,7 +3,8 @@ package dk.wiingreen.starfinder.auth;
 import jakarta.validation.constraints.NotBlank;
 
 record RegisterUserRequest(
-    String username, @NotBlank(message = "Password is required") String password) {
+    @NotBlank(message = "Username is required") String username,
+    @NotBlank(message = "Password is required") String password) {
   @Override
   public String toString() {
     return "RegisterUserRequest{username='%s'}".formatted(password);
