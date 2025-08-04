45bf9bb5c04016ece4f6943941867f4242bfa4ae
Claus Ryberg Wiingreen <claus@wiingreen.dk>
2025-08-04 23:13:34 +0200

    feat: enforce minimum password length requirement during registration

--- src/main/java/dk/wiingreen/starfinder/auth/RegisterUserRequest.java
+++ src/main/java/dk/wiingreen/starfinder/auth/RegisterUserRequest.java
@@ -1,10 +1,14 @@
 package dk.wiingreen.starfinder.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 record RegisterUserRequest(
     @NotBlank(message = "Username is required") String username,
-    @NotBlank(message = "Password is required") String password) {
+    @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password) {
+
   @Override
   public String toString() {
     return "RegisterUserRequest{username='%s'}".formatted(password);
