b548a0b285a84ece959d41c0224cd07c15a05c2d
Claus Ryberg Wiingreen <claus@wiingreen.dk>
2025-08-07 22:30:13 +0200

    feat: persist new campaigns in CampaignController
    Updates CampaignController to save a new Campaign entity via
    CampaignRepository when handling POST requests to /campaigns.
    This connects the web layer to the persistence layer, enabling actual
    storage of campaign data.

--- src/main/java/dk/wiingreen/starfinder/campaign/CampaignController.java
+++ src/main/java/dk/wiingreen/starfinder/campaign/CampaignController.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/campaigns")
 public class CampaignController {
+  private final CampaignRepository campaignRepository;
+
+  public CampaignController(CampaignRepository campaignRepository) {
+    this.campaignRepository = campaignRepository;
+  }
+
   @PostMapping
   String createCampaign() {
+    campaignRepository.save(new Campaign());
     return "redirect:/campaigns";
   }
 }
